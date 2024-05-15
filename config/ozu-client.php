1130703afded471e00b96ac916ef95aa145c137a
philippe <philippe@code16.fr>
2024-05-15 11:49:07 +0200

    wip

--- config/ozu-client.php
+++ config/ozu-client.php
@@ -3,7 +3,7 @@
 return [
     'api_host' => env('OZU_API_HOST', 'http://ozu.test/'),
     'api_key' => env('OZU_API_KEY'),
-    'api_version' => env('OZU_API_VERSION', 'v3'),
+    'api_version' => env('OZU_API_VERSION', 'v1'),
     'website_key' => env('OZU_WEBSITE_KEY'),
     'collections' => [
         \App\Models\Page::class,
