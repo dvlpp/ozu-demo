456afbe79a9e5f890823b863fdd06345117f0985
philippe <philippe@code16.fr>
2024-06-13 09:28:22 +0200

    Use slug

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::get('/', HomePageController::class)
 Route::get('/projects/all/{page?}', [ProjectController::class, 'index'])
     ->name('projects.index');
 
-Route::get('/projects/{project}', [ProjectController::class, 'show'])
+Route::get('/projects/{project:slug}', [ProjectController::class, 'show'])
     ->name('projects.show');
 
 Route::get('/about', AboutPageController::class)
